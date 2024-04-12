299f17e132710144c339919a829bc059fe41d227
Andreas Hohwü-Christensen <andreas.hohwu@gmail.com>
2024-04-12 10:10:28 +0200

    Add project files.

---
+++ Storage/Controllers/ProductsController.cs
@@ -0,0 +1,187 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Storage.Data;
+using Storage.Models;
+
+namespace Storage.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly StorageContext _context;
+
+        public ProductsController(StorageContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Product.ToListAsync());
+        }
+
+        public async Task<IActionResult> Summaries()
+        {
+            IEnumerable<Product> rawData = await _context.Product.ToListAsync();
+            IEnumerable<ProductViewModel> summaries = rawData.Select(product => new ProductViewModel() { Name = product.Name, Price = product.Price, Count = product.Count, InventoryValue = product.Price * product.Count });
+            return View(summaries);
+        }
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Price,OrderDate,Category,Shelf,Count,Description")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Price,OrderDate,Category,Shelf,Count,Description")] Product product)
+        {
+            if (id != product.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var product = await _context.Product.FindAsync(id);
+            if (product != null)
+            {
+                _context.Product.Remove(product);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Search()
+        {
+            await PrepareViewBagForCategorySearch();
+            IEnumerable<Product> pseudoSearchResult = new List<Product>();
+            return View(pseudoSearchResult);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Search(string category, string name)
+        {
+            await PrepareViewBagForCategorySearch();
+            IEnumerable<Product> searchResult = await _context.Product.ToListAsync();
+            if (!string.IsNullOrEmpty(category))
+                searchResult = searchResult.Where((Product product) => product.Category == category);
+            if (!string.IsNullOrEmpty(name))
+                searchResult = searchResult.Where((Product product) => product.Name.Contains(name, StringComparison.CurrentCultureIgnoreCase));
+            return View(searchResult);
+        }
+
+        private async Task PrepareViewBagForCategorySearch()
+        {
+            ViewBag.AvailableCategories = (await _context.Product.ToListAsync()).Select(product => product.Category).Distinct().Select(categoryString => new SelectListItem(categoryString, categoryString)).ToList();
+            ViewBag.AvailableCategories.Insert(0, new SelectListItem("", ""));
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Product.Any(e => e.Id == id);
+        }
+
+    }
+}
---
+++ Storage/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Storage.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required, Range(1, Int32.MaxValue)]
+        public int Price { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime OrderDate { get; set; }
+        public string Category { get; set; }
+        public string Shelf { get; set; }
+        [Required, Range(0, Int32.MaxValue)]
+        public int Count { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
